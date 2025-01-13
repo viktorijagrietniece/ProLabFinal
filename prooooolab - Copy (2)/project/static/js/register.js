cf4bd6792b7240d68f9c5cb4b782424e00da701d
viktorija.g <grietniece2003@gmail.com>
2025-01-13 07:24:51 +0200

    Code cleanup, izņēmu dažas neizmantotās lietas un salaboju reģistrācijas redirect uz login page

--- prooooolab - Copy (2)/project/static/js/register.js
+++ prooooolab - Copy (2)/project/static/js/register.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const registerForm = document.getElementById("registerForm");
-    const messageBox = document.getElementById("messageBox");
     registerForm.addEventListener("submit", async (event) => {
         event.preventDefault();
 
@@ -11,36 +10,30 @@ document.addEventListener("DOMContentLoaded", () => {
         const confirmPassword = document.getElementById("confirmPassword").value;
         const phone = document.getElementById("phone").value.trim();
         const role = document.getElementById("role").value;
-        const department = document.getElementById("department").value;
 
-        messageBox.innerHTML = "";
         if (password !== confirmPassword) {
-            messageBox.innerHTML = `<p style="color: red;">Passwords do not match!</p>`;
+            alert("Passwords do not match!");
             return;
         }
+
         try {
             const response = await fetch("http://localhost:5000/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ name, lastname, email, password, phone, role, department })
+                body: JSON.stringify({ name, lastname, email, password, phone, role})
             });
+
             if (response.status === 201) {
-                messageBox.innerHTML = `
-                    <p style="color: green;">Registration successful!</p>
-                    <button id="loginRedirect">Go to Login</button>
-                `;
-                document.getElementById("loginRedirect").addEventListener("click", () => {
-                    window.location.href = "/index.html";
-                });
+                window.location.href = "/index.html";
             } else {
                 const result = await response.json();
-                messageBox.innerHTML = `<p style="color: red;">${result.message || "Registration failed. Please try again."}</p>`;
+                alert(result.message || "Registration failed. Please try again.");
             }
         } catch (error) {
             console.error("Error registering:", error);
-            messageBox.innerHTML = `<p style="color: red;">An error occurred. Please try again.</p>`;
+            alert("An error occurred. Please try again.");
         }
     });
 });
